66e201dc3c653b4ed19a34089dd02ccfa1d7782a
Ripon Ahmed <riponahmed2201@gmail.com>
2022-02-24 13:51:55 +0600

    product dynamic feild issue added

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -2,7 +2,6 @@ import { Request, Response } from "express";
 import _, { get, split } from "lodash";
 import Product from "../model/product.model";
 import { createProduct, getAllProduct } from "../service/product.service";
-import product from "../utils/product";
 import productPermission from "../utils/product"
 
 export default async function createProductHandler(req: Request, res: Response) {
@@ -15,39 +14,43 @@ export default async function createProductHandler(req: Request, res: Response) 
 
 export async function getProductHandler(req: Request, res: Response) {
     try {
-        let getAllProduct = await Product.find();
-        console.log("Product :", getAllProduct);
+        const userSubscriptionType = 'basic';
 
-        const refineProduct = await refineBySubscription(getAllProduct, 'premium');
+        const getFeildObject = await productPermission('products',userSubscriptionType);
+        let getAllProduct = await Product.find({},getFeildObject);
+
+        // const refineProduct = await refineBySubscription(getAllProduct, userSubscriptionType);
 
         res.status(200).json({
             status: 'success',
-            data: refineProduct
+            statusCode:200, 
+            results: getAllProduct
         });
 
     } catch (error: any) {
         res.status(500).json({
             status: 'error',
+            statusCode:500,
             error: error.message
         });
     }
 }
 
-const refineBySubscription = async (products: any, subscriptionType: string) => {
-    const data = await findSubscriptionFeilds(subscriptionType);
-    if (data) {
-        let productData: any = [];
-        _.forEach(products, function (product) {
-            productData.push(_.pick(product, data.fields));
-        });
-        return productData;
-    } else {
-        return [];
-    }
-}
+// const refineBySubscription = async (products: any, subscriptionType: string) => {
+//     const data = await findSubscriptionFeilds(subscriptionType);
+//     if (data) {
+//         let productData: any = [];
+//         _.forEach(products, function (product) {
+//             productData.push(_.pick(product, data.fields));
+//         });
+//         return productData;
+//     } else {
+//         return [];
+//     }
+// }
 
-const findSubscriptionFeilds = async (subscriptionType: String) => {
-    return _.find(productPermission, (info) => {
-        return info && info.subscriptionType === subscriptionType;
-    });
-};
+// const findSubscriptionFeilds = async (subscriptionType: String) => {
+//     return _.find(productPermission, (info) => {
+//         return info && info.subscriptionType === subscriptionType;
+//     });
+// };
--- src/utils/product.ts
+++ src/utils/product.ts
@@ -1,36 +1,55 @@
- const productPermission = [
-    {
-        "subscriptionType": "basic",
-        "fields": [
-            "name",
-            "description"
-        ]
-    },
+import _ from "lodash";
 
-    {
-        "subscriptionType": "standard",
-        "fields": [
-            "name",
-            "supplierPrice",
-            "mrpPrice",
-            "discountedPrice"
-        ]
+export const permissionList = [
+  {
+    subscriptionType: "basic",
+    collectionName: "products",
+    fields: {
+      name: 1,
+      description: 1,
     },
+  },
 
-    {
-        "subscriptionType": "premium",
-        "fields": [
-            "name",
-            "shortDescription",
-            "description",
-            "supplierPrice",
-            "mrpPrice",
-            "discountedPrice",
-            "stockTotal",
-            "categoryName",
-            "code"
-        ]
+  {
+    subscriptionType: "standard",
+    collectionName: "products",
+    fields: {
+      name: 1,
+      supplierPrice: 1,
+      mrpPrice: 1,
+      discountedPrice: 1,
+    },
+  },
+
+  {
+    subscriptionType: "premium",
+    collectionName: "products",
+    fields: {
+      name: 1,
+      shortDescription: 1,
+      description: 1,
+      supplierPrice: 1,
+      mrpPrice: 1,
+      discountedPrice: 1,
+      stockTotal: 1,
+      categoryName: 1,
+      code: 1,
+    },
+  },
+];
+
+const productPermission = async (
+  collectionName: String,
+  subscriptionType: string
+) => {
+  let data = {};
+  _.find(permissionList, (info) => {
+    if (info.subscriptionType === subscriptionType) {
+      data = info.fields;
     }
-]
+  });
+
+  return data;
+};
 
 export default productPermission;
